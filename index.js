83fbc3ee01ff0deb497a8907fc250ec7048d7d81
Stacey Louis <staceylouis7@gmail.com>
2023-02-14 17:45:59 -0500

    added server documentation

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose")
 const cookieParser = require('cookie-parser');
 const app = express()
 
+const todoRouter = require('./routes/todo')
+
 const PORT = process.env.PORT || 8080
 
 app.use(express.static('public'));
@@ -14,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(logger('dev'));
 
-
+app.use('/todos', todoRouter)
 const string = process.env.MONGO_URI
 mongoose.connect(string)
 
@@ -25,6 +27,7 @@ database.on('error:', error=>{
 database.once('connected',()=>{
     console.log("Database Connected")
 })
+
 
 app.listen(PORT, (req,res)=>{
     console.log(`Server listening on port ${PORT}`)
